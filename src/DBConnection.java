161839a11feeb9d17e2a32377ad753e4680bc2e2
unknown <stanleyaloysius007@gmail.com>
2024-09-11 20:18:11 +0530

    DB Connection

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,3 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
-    
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                String url = "jdbc:mysql://localhost:3306/library_db";
+                String username = "user";
+                String password = "pa$$w0rd";  
+                connection = DriverManager.getConnection(url, username, password);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
 }
